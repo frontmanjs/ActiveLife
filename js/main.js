277a89526b22a78406be2923dcab938ea7c1e5d9
Jan <jzawadzki200@gmail.com>
2023-09-15 21:58:57 +0200

    to eng

--- js/main.js
+++ js/main.js
@@ -12,30 +12,30 @@ const calc = () => {
 	bmi = weight.value / ((height.value * height.value) / 10000);
 	if (bmi >= 40) {
 		resultMore.textContent =
-			'Otyłość lll stopnia - Wynik ten wskazuje na bardzo znaczną nadwagę, co może prowadzić do poważnych problemów zdrowotnych i wymaga natychmiastowej interwencji medycznej.';
+			'Grade lll obesity - This score indicates very significant overweight, which can lead to serious health problems and requires immediate medical intervention.';
 	} else if (bmi < 39.99 && bmi > 35) {
 		resultMore.textContent =
-			'Otyłość ll stopnia - To umiarkowanie znacząca nadwaga, która również niesie ze sobą ryzyko zdrowotne, więc zaleca się konsultację z lekarzem w celu opracowania planu odchudzania.';
+			'Degree ll obesity - This is moderately significant overweight that also carries health risks, so it is advisable to consult a doctor to develop a weight loss plan.';
 	} else if (bmi < 34.99 && bmi > 30) {
 		resultMore.textContent =
-			'Otyłość l stopnia - Stan ten wskazuje na niewielką nadwagę, która może być kontrolowana przez zmianę stylu życia, takie jak dieta i regularna aktywność fizyczna.';
+			'Obesity of l degree - This condition indicates being slightly overweight, which can be controlled by lifestyle changes such as diet and regular physical activity.';
 	} else if (bmi < 29.99 && bmi > 25) {
 		resultMore.textContent =
-			'Nadwaga - Oznacza to, że masz nieco więcej tkanki tłuszczowej niż to, co jest uważane za zdrowe. Warto rozważyć modyfikację nawyków żywieniowych i aktywności fizycznej.';
+			'Overweight - This means that you have slightly more body fat than what is considered healthy. You should consider modifying your eating habits and physical activity.';
 	} else if (bmi < 24.99 && bmi > 18.5) {
 		resultMore.textContent =
-			'Waga prawidłowa - Twój wynik BMI jest w zakresie normy, co sugeruje, że utrzymujesz zdrową wagę. Nadal ważne jest utrzymywanie zdrowego stylu życia.';
+			'Normal weight - Your BMI score is within the normal range, suggesting that you are maintaining a healthy weight. It is still important to maintain a healthy lifestyle.';
 	} else if (bmi < 18.5 && bmi > 17) {
 		resultMore.textContent =
-			'Niedowaga - Oznacza to, że Twoja waga jest niższa niż norma, co może być niezdrowe. Warto skonsultować się z lekarzem, aby ustalić przyczyny i podjąć odpowiednie kroki.';
+			'Underweight - This means that your weight is lower than the norm, which can be unhealthy. It is a good idea to consult a doctor to determine the causes and take appropriate measures.';
 	} else if (bmi < 16 && bmi > 16.99) {
 		resultMore.textContent =
-			'Wychudzenie -  Stan ten wskazuje na bardzo niską masę ciała, co jest niebezpieczne dla zdrowia. Wymaga natychmiastowej opieki medycznej i monitorowania.';
+			'Emaciation - This condition indicates very low body weight, which is dangerous to health. It requires immediate medical care and monitoring.';
 	} else if (bmi >= 15.99) {
 		resultMore.textContent =
-			'Wygłodzenie - To krytyczny stan wynikający z skrajnego niedożywienia, który może zagrażać życiu. Konieczna jest natychmiastowa interwencja medyczna w celu przywrócenia zdrowia.';
+			'Starvation - This is a critical condition resulting from extreme malnutrition that can be life-threatening. Immediate medical intervention is needed to restore health.';
 	}
-	result.textContent = `Twoje BMI to ${bmi.toFixed(2)}`;
+	result.textContent = `Your BMI is ${bmi.toFixed(2)}`;
 	
 };
 
@@ -43,7 +43,7 @@ const calcBmi = () => {
 	if (weight.value !== '' && height.value !== '') {
 		calc();
 	} else {
-		result.textContent = 'Brak wpisanej wartości';
+		result.textContent = 'No value entered';
 		resultMore.textContent = ''
 	}
 	border.style.opacity = '1';
